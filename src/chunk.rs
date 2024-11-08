9c33e1d3ac98470fb2f7b9be3c9728c1a853b760
Andrés <nunezlopez.ja@gmail.com>
2024-11-08 12:53:16 -0600

    funny, I'm having problems again

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,4 +1,7 @@
-use std::{fmt::Display, io::{BufReader, Read}};
+use std::{
+    fmt::Display,
+    io::{BufReader, Read},
+};
 
 use crc::{Crc, CRC_32_ISO_HDLC};
 
@@ -11,18 +14,29 @@ struct Chunk {
     crc: u32,
 }
 
+#[allow(dead_code)]
 impl Chunk {
     fn new(chunk_type: ChunkType, data: Vec<u8>) -> Chunk {
         let length: u32 = data.len().try_into().unwrap();
-        let crc_bytes: Vec<u8> = chunk_type.chunk_type.clone().into_iter().chain(data.iter().cloned()).collect();
+        let crc_bytes: Vec<u8> = chunk_type
+            .chunk_type
+            .clone()
+            .into_iter()
+            .chain(data.iter().cloned())
+            .collect();
         let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
         let calculated_crc = crc.checksum(&crc_bytes);
         let provided_crc = u32::from_be_bytes(crc_bytes.try_into().unwrap());
 
         if calculated_crc != provided_crc {
             panic!("CRC mismatch when creating by ::new");
-        } 
-        Self { length, chunk_type, chunk_data: data, crc: calculated_crc }
+        }
+        Self {
+            length,
+            chunk_type,
+            chunk_data: data,
+            crc: calculated_crc,
+        }
     }
 
     fn length(&self) -> u32 {
@@ -42,8 +56,13 @@ impl Chunk {
     }
 
     fn as_bytes(&self) -> Vec<u8> {
-        let bytes = self.chunk_type.chunk_type.clone()
-            .into_iter().chain(self.chunk_data.clone()).collect();
+        let bytes = self
+            .chunk_type
+            .chunk_type
+            .clone()
+            .into_iter()
+            .chain(self.chunk_data.clone())
+            .collect();
         bytes
     }
 }
@@ -55,7 +74,7 @@ impl Display for Chunk {
         writeln!(f, "Chunk Type: {}", self.chunk_type)?;
         writeln!(f, "Data: {}", self.chunk_data.len())?;
         writeln!(f, "Crc: {}", self.crc)?;
-        write!(f, "}}");
+        write!(f, "}}")?;
         Ok(())
     }
 }
@@ -72,34 +91,47 @@ impl TryFrom<&[u8]> for Chunk {
         // Read the first 4 bytes to determine data length
         let mut buffer: [u8; 4] = [0, 0, 0, 0];
         reader.read_exact(&mut buffer)?;
-        const DATA_LENGTH: u32 = u32::from_be_bytes(buffer);
+        let data_length: u32 = u32::from_be_bytes(buffer);
 
         // Read next 4 bytes to determine chunk type
         let mut chunk_type = [0u8; 4];
         reader.read_exact(&mut chunk_type)?;
 
         // Read n bytes to determine data
-        let mut data = [0u8; DATA_LENGTH as usize];
+        //let mut data = [0u8; data_length as usize];
+        let mut data = vec![0u8; data_length as usize];
         reader.read_exact(&mut data)?;
 
         // Take chunk_data and chunk_type for CRC
         let mut crc_bytes = [0u8; 4];
         reader.read_exact(&mut crc_bytes)?;
 
-        let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
-        let calculated_crc = crc.checksum(&crc_bytes);
-        let provided_crc = u32::from_be_bytes(crc_bytes.try_into()?);
+        let provided_crc = u32::from_be_bytes(crc_bytes);
+
+        // Calculate crc over entire chunk
+        let mut crc_data = Vec::with_capacity(8 + data.len());
+        crc_data.extend_from_slice(&data_length.to_be_bytes());
+        crc_data.extend_from_slice(&chunk_type);
+        crc_data.extend_from_slice(&data);
+
+        let calculated_crc = Crc::<u32>::new(&CRC_32_ISO_HDLC).checksum(&crc_data);
 
         if calculated_crc != provided_crc {
             return Err("CRC mismatch".into());
         }
 
-        Ok(Self { length: data_length, chunk_type: ChunkType { chunk_type: chunk_type.to_vec() }, chunk_data: data, crc: calculated_crc })
-
+        Ok(Self {
+            length: data_length,
+            chunk_type: ChunkType {
+                chunk_type: chunk_type.to_vec(),
+            },
+            chunk_data: data,
+            crc: calculated_crc,
+        })
     }
 }
 
-#![allow(unused_variables)]
+#[allow(dead_code)]
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -150,7 +182,8 @@ mod tests {
     #[test]
     fn test_chunk_string() {
         let chunk = testing_chunk();
-        let chunk_string = chunk.data_as_string().unwrap();
+        //let chunk_string = chunk.data_as_string().unwrap();
+        let chunk_string = chunk.data_as_string();
         let expected_chunk_string = String::from("This is where your secret message will be!");
         assert_eq!(chunk_string, expected_chunk_string);
     }
@@ -179,7 +212,8 @@ mod tests {
 
         let chunk = Chunk::try_from(chunk_data.as_ref()).unwrap();
 
-        let chunk_string = chunk.data_as_string().unwrap();
+        //let chunk_string = chunk.data_as_string().unwrap();
+        let chunk_string = chunk.data_as_string();
         let expected_chunk_string = String::from("This is where your secret message will be!");
 
         assert_eq!(chunk.length(), 42);
